02d0adb53c96da17cdc5944ddfa68fefed7ddbbb
kirie0319 <matsueda0407@icloud.com>
2024-06-29 12:03:56 +0900

    my girlfriend

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 
 import streamlit as st
 import openai
+import re
+
+# ファイルを読み込み
+with open('/Users/yuya0407/Desktop/Python/openaichatbot/[LINE] Chat with Minami Otsuki.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+# データの抽出と前処理
+cleaned_lines = []
+for line in lines:
+    # 日付や不要な部分を除去
+    cleaned_line = re.sub(r'\d{2}:\d{2}[AP]M\s+\w+\s+.*\n', '', line)
+    cleaned_lines.append(cleaned_line.strip())
+
+# 前処理されたテキストデータ
+cleaned_text = "\n".join(cleaned_lines)
+
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
@@ -8,7 +24,7 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
+        {"role": "system", "content": "あなたは私の彼女です。"}
         ]
 
 # チャットボットとやりとりする関数
