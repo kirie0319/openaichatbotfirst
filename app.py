7fdbeef808e54812f3fba348c06306274b872e26
kirie0319 <matsueda0407@icloud.com>
2024-06-29 20:38:11 +0900

    my girlfriend3

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import openai
 import re
@@ -19,13 +18,13 @@ cleaned_text = "\n".join(cleaned_lines)
 
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
-openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+openai.api_key = st.secrets["OpenAIAPI"]["openai_api_key"]
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": "あなたは私の彼女です。"}
-        ]
+    ]
 
 # チャットボットとやりとりする関数
 def communicate():
@@ -44,7 +43,6 @@ def communicate():
 
     st.session_state["user_input"] = ""  # 入力欄を消去
 
-
 # ユーザーインターフェイスの構築
 st.title("My AI Assistant")
 st.write("ChatGPT APIを使ったチャットボットです。")
@@ -56,7 +54,7 @@ if st.session_state["messages"]:
 
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
         speaker = "🙂"
-        if message["role"]=="assistant":
-            speaker="🤖"
+        if message["role"] == "assistant":
+            speaker = "🤖"
 
         st.write(speaker + ": " + message["content"])
